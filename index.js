43afa1bd4b669950c312c6339bc768e4ddacd1ab
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-03 04:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function lowerCaseDrivers(array){
   return array.map(function(e){
     return e.toLowerCase()
   })
-};
+
+
+function nameToAttributes(names){
+ let fName = name.split (" ") [0]
+ let lName = name.split (" ") [1]
+ return {firstName: fName, lastName: lName}
+
+ })
+
+  
