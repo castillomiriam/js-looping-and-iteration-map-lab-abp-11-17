b37c7e40e76df9d5114eff70a5fba62c89b976ee
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-03 04:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function lowerCaseDrivers(array){
-  return array.map(function(e){
-    return e.toLowerCase()
+  return array.map(function(driver){
+    return driver.toLowerCase()
   })
 };
 
 function nameToAttributes(names){
-  return names.map(function(name){
+return names.map(function(name){
  let fName = name.split(" ")[0]
  let lName = name.split(" ")[1]
  return {firstName: fName, lastName: lName}
